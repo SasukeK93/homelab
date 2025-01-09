d4f0f77318250dbf0d060e62040e0aa2e24e24dc
Jose Fuxa <josea.fuxa@gmail.com>
2025-01-09 08:18:55 -0500

    Refactor Terraform configuration: Rename vSwitch, update VLAN ID for portgroup, and streamline master node resource definition while maintaining existing settings.

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,37 +8,32 @@ provider "esxi" {
 
 # Network Configuration
 resource "esxi_vswitch" "k3s_network" {
-  name = "Kubernetes Network Switch"
-  uplink {
-    name = "vusb0"  # Uncomment and adjust based on your ESXi host's network interface (vmnic1)
-  }
-  
-  mtu        = 9000   # Jumbo frames for better network performance
+  name = "vKSwitch"
+  mtu  = 9000 # Jumbo frames for better network performance
 }
 
 resource "esxi_portgroup" "k3s_portgroup" {
   name    = "Kubernetes Network"
   vswitch = esxi_vswitch.k3s_network.name
-  #vlan    = 4040     # Adjust VLAN ID as needed
+  vlan    = 20
 }
 
 # Master Node Configuration
 resource "esxi_guest" "k8s_master" {
   guest_name    = "k8s-master"
   disk_store    = var.datastore
-  ovf_source    = "https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64.ova"
-  #clone_from_vm     = "ubuntu-20.04-template"
+  ovf_source    = "/home/ubuntu/homelab/ubuntu-fresh/ubuntu-20.04-template.ovf"
   guestos       = "ubuntu-64"
   boot_firmware = "efi"
 
   memsize   = "4096"
   numvcpus  = "2"
   power     = "on"
-  virthwver = "19"  # Hardware version compatible with ESXi
+  virthwver = "19" # Hardware version compatible with ESXi
 
   network_interfaces {
     virtual_network = esxi_portgroup.k3s_portgroup.name
-    nic_type       = "vmxnet3"  # High performance network adapter
+    nic_type       = "vmxnet3" # High performance network adapter
   }
 
   guest_startup_timeout  = 180
