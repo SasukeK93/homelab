6c4c045c73a595151d9fa97907788428d732f630
Jose Fuxa <josea.fuxa@gmail.com>
2025-01-09 08:05:52 -0500

    Refactor Terraform configuration: Remove unnecessary provider block, add ESXi network resources for Kubernetes, update OVF source for master node, and streamline network interface references.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    esxi = {
-      source  = "registry.terraform.io/josenk/esxi"
-      version = "~> 1.10.3"
-    }
-  }
-}
-
 provider "esxi" {
   esxi_hostname = var.esxi_hostname
   esxi_hostport = var.esxi_hostport
@@ -15,20 +6,27 @@ provider "esxi" {
   esxi_password = var.esxi_password
 }
 
-# Cloud Init Configuration
-data "template_file" "Master" {
-  template = file("userdata-master.tpl")
-  vars = {
-    HOSTNAME = var.vm_hostname
-    HELLO    = "Hello World!"
+# Network Configuration
+resource "esxi_vswitch" "k3s_network" {
+  name = "Kubernetes Network Switch"
+  uplink {
+    name = "vusb0"  # Uncomment and adjust based on your ESXi host's network interface (vmnic1)
   }
+  
+  mtu        = 9000   # Jumbo frames for better network performance
+}
+
+resource "esxi_portgroup" "k3s_portgroup" {
+  name    = "Kubernetes Network"
+  vswitch = esxi_vswitch.k3s_network.name
+  #vlan    = 4040     # Adjust VLAN ID as needed
 }
 
 # Master Node Configuration
 resource "esxi_guest" "k8s_master" {
   guest_name    = "k8s-master"
   disk_store    = var.datastore
-  ovf_source    = "~/homelab/ubuntu-fresh/ubuntu-20.04-template.ovf"
+  ovf_source    = "https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64.ova"
   #clone_from_vm     = "ubuntu-20.04-template"
   guestos       = "ubuntu-64"
   boot_firmware = "efi"
@@ -39,18 +37,13 @@ resource "esxi_guest" "k8s_master" {
   virthwver = "19"  # Hardware version compatible with ESXi
 
   network_interfaces {
-    virtual_network = "VM Network" #esxi_portgroup.k3s_portgroup.name
+    virtual_network = esxi_portgroup.k3s_portgroup.name
     nic_type       = "vmxnet3"  # High performance network adapter
   }
 
   guest_startup_timeout  = 180
   guest_shutdown_timeout = 60
   ovf_properties_timer   = 180
-
-  guestinfo = {
-    "userdata.encoding" = "gzip+base64"
-    "userdata"         = base64gzip(data.template_file.Master.rendered)
-  }
 
   provisioner "local-exec" {
     command = "echo 'Master node ${self.guest_name} has been created with IP: ${self.ip_address}'"
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,9 @@
 
 terraform {
-  required_version = ">= 0.13"
   required_providers {
     esxi = {
-      source = "registry.terraform.io/josenk/esxi"
+      source = "registry.terraform.io/josenk/esxi",
+      version = "~> 1.10.3"
     }
   }
 }
